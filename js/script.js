0aa072741b0824c351c49543410f49038e4fc004
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-03-15 20:49:11 +0300

    add html,css and begin developing js functions

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ var onload="alert('Hello, I am your pet rock.');" function touchRock();
+  {
+  
+  function touchRock() {
+  var userName = prompt("What is your name?", "Enter your name here.");
+  if (userName) {
+  alert("It is good to meet you, " + userName + ".");
+} document.getElementById("rockImg").src = "rock_happy.png";  
+} 
